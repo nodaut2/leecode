93f60d4f092620e0af775b7ff3fc879488b568da
lee <full_set@naver.com>
2016-11-07 14:19:40 +0900

    20161107

--- gittest/test.py
+++
@@ -1,8 +0,0 @@
-print "Test in my linux"
-print "Test in my linux2"
-print "Test in my linux3"
-print "Test in my mac"
-print "Test in my mac"
-print "Test in my mac"
-print "Test in my mac"
-print "Test in my mac"
